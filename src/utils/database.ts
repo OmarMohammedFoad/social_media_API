58016c2a6a7d28201126785e8c509811f12d5f4a
Omar Muhammad <cds.omarmuhammad92694@alexu.edu.eg>
2025-05-02 16:50:59 +0300

    add uploads

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { config } from "dotenv";
 config();
 export const connect = async () => {
-  console.log("data connected");
+  console.log("data connecteddd");
   
   await mongoose
 
